dbf7f0b81a248d003465011b65c042a2323968fd
Kameron <kamahler@gmail.com>
2020-11-08 14:45:34 -0700

    scaffold list item

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,16 +3,18 @@ import React from 'react'
 export default function List() {
   return (
     <ul className='list'>
-      <li className='list__item'>1</li>
-      <li className='list__item'>2</li>
-      <li className='list__item'>3</li>
-      <li className='list__item'>4</li>
-      <li className='list__item'>5</li>
-      <li className='list__item'>6</li>
-      <li className='list__item'>7</li>
-      <li className='list__item'>8</li>
-      <li className='list__item'>9</li>
-      <li className='list__item'>10</li>
+      <li className='list__item'>
+        <span className='list__item-controls'>
+          <input className='list__item-checkbox' type='checkbox' />
+        </span>
+        <h4 className='list__item-title'>Lorem, ipsum dolor</h4>
+        <p className='list__item-preview'>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur
+          ipsum repellat nesciunt architecto, dolore exercitationem, quia
+          consequatur quibusdam iusto dicta eaque quidem iste omnis quaerat hic
+          eius atque, earum fugit?
+        </p>
+      </li>
     </ul>
   )
 }
